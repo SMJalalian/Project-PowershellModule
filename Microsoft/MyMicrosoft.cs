650916333695a364191379d940859ba308519f3a
Mohamad Jalalian <SM.jalalian@outlook.com>
2019-04-05 12:28:36 +0000

    Add Some Microsoft Commands Imported from Changeset 55

--- Microsoft/MyMicrosoft.cs
+++ Microsoft/MyMicrosoft.cs
@@ -3,12 +3,36 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Management.Automation;
+using System.Runtime.InteropServices;
+using System.Security;
 
 namespace Microsoft
-{
+{    
     public class MyMicrosoft : Cmdlet
     {
+        [Parameter()]
+        public SwitchParameter AutomationLog { set; get; }
+
         public string LogCategory { get; private set; } = "Microsoft";
+
+        public string SQLServerName { get; set; }
+        public string SQLServerInstance { get; set; }
+        public string SQLDB { get; set; }
+        public string AssemblyFolder { get; set; }
+
+        public string SecureStringToString(SecureString value)
+        {
+            IntPtr valuePtr = IntPtr.Zero;
+            try
+            {
+                valuePtr = Marshal.SecureStringToGlobalAllocUnicode(value);
+                return Marshal.PtrToStringUni(valuePtr);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(valuePtr);
+            }
+        }
     }
 
     public class PSEventLog
@@ -51,4 +75,5 @@ namespace Microsoft
         public string User { get; set; }
         public string Action { get; set; }
     }
+
 }
