5556b16c3fecd4509b4b7651ed38c8d140d67bd6
Mohamad Jalalian <SM.jalalian@outlook.com>
2019-06-05 15:04:15 +0000

    Change Structure Imported from Changeset 77

---
+++ Z_TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Z_TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
